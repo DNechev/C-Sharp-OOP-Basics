1fc6ad23d621c5bc16446866d374bd40e2ee05ff
DNechev <dido94m@gmail.com>
2019-06-25 13:23:32 +0200

    Initial commit

---
+++ 05. Interfaces and Abstraction Exercises/01. Define an Interface IPerson/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+ public class Program
+{
+    public static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        IPerson person = new PersonInfo(name, age);
+        Console.WriteLine(person.Name);
+        Console.WriteLine(person.Age);
+    }
+}
